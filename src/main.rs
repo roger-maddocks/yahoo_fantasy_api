b0eb75fc04846f1c3cdc8602b523758b12838c19
Roger Maddocks <roger.maddocks@gmail.com>
2023-11-07 09:37:41 -0700

    >WIP Yahoo Auth early stages ROLLBACK but works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use reqwest::Error;
+use reqwest::{Client, ClientBuilder};
 use crate::fantasy_week::FantasyWeek;
 use crate::team::Team;
 pub mod roster_builder;
@@ -11,13 +12,47 @@ mod weekly_data_factory;
 mod report;
 mod fantasy_week;
 mod collision_report;
+mod yahoo_auth_profile;
 
 #[tokio::main]
 async fn main () -> Result<(), Error> {
-    for i in 4 ..= 4  {
+    for i in 5 ..= 5  {
         let this_week = FantasyWeek::new(i, i);
         weekly_data_factory::get_loaded_schedule_report(i, &this_week).await;
     }
 
     Ok(())
 }
+//
+// pub struct YahooConnector {
+//     client: Client,
+//     url: &'static str,
+//     search_url: &'static str,
+// }
+//
+// #[derive(Default)]
+// pub struct YahooConnectorBuilder {
+//     inner: ClientBuilder,
+// }
+//
+// impl YahooConnector {
+//
+//     pub fn new() -> YahooConnector {
+//         YahooConnector {
+//             client: Client::default(),
+//             url: env![""],
+//             search_url: env![""],
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
